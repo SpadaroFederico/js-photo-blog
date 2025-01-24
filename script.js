5cbb19379de92ea7823eca3a06927067609381f0
Federico Spadaro <federicospadaro2@gmail.com>
2025-01-24 13:05:02 +0100

    [ADD] API call + first test with js

---
+++ script.js
@@ -0,0 +1,37 @@
+// Seleziono gli elementi di input
+const ContainerOfCard = document.querySelector('.container');
+
+// setto ora l'endpoint e i parametri
+const endpoint = `https://lanciweb.github.io/demo/api/pictures/`
+
+// richiesta ajax verso API
+axios.get(endpoint)
+    .then(responseObj => {
+
+        // se funziona esegui:
+        const posts = responseObj.data;
+        console.log(posts);
+
+        for (let i = 0; i < posts.length; i++) {
+            let SinglePost = posts[i];
+            console.log(SinglePost);
+            
+            // destructuring oggetto
+
+            const {title, date, url} = SinglePost;
+
+            // creo le singole card
+            ContainerOfCard.innerHTML += `
+                <div class="card">
+                    <h2 class="CardTitle">${title}</h2>
+                    <p class="CardDate">${date}</p>
+                    <span class="imageUrl">${url}</span>
+                </div>
+            ` 
+        }
+        
+    })
+    .catch(error => {
+        // se non funzina esegui:
+        console.error(error)
+    })
